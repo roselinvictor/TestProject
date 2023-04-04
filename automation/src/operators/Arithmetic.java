007e11ab35498b0fdfe3a18a028a17a4c4cfc39c
vicky <vicky@DESKTOP-FDFLRKC.attlocal.net>
2023-04-04 13:25:41 -0500

    Adding the Testproject

---
+++ automation/src/operators/Arithmetic.java
@@ -0,0 +1,26 @@
+package operators;
+
+public class Arithmetic {
+
+	public static void main(String[] args) {
+		
+		int a = 10;
+		int b = 2;
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+		
+		
+		System.out.println(100+100);
+		System.out.println(100-100);
+		System.out.println(100*100);
+		System.out.println(100/100);
+		
+		String s1 = "abc";
+		String s2 = "efg";
+		System.out.println(s1+s2);
+	}
+
+}
---
+++ automation/src/operators/Relationaloperator.java
@@ -0,0 +1,19 @@
+package operators;
+
+public class Relationaloperator {
+
+	public static void main(String[] args) {
+
+		int a = 10;
+		int b = 8;
+
+		System.out.println(a < b);
+		System.out.println(a <= b);
+		System.out.println(a > b);
+		System.out.println(a >= b);
+		System.out.println(a = b);
+		System.out.println(a != b);
+
+	}
+
+}
